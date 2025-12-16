3755d0d4f7106f8f15f19ca5298b0e9d0e4d1494
V3nusAsABoy <andrewalloniealt@gmail.com>
2025-12-16 18:04:11 -0500

    added post and get requests for drawing

--- server/api.js
+++ server/api.js
@@ -3,10 +3,36 @@ const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const multer = require('multer');
+const uploadMiddleware = multer({dest: 'uploads/'});
+const fs = require('fs');
+const Drawing = require('./dbmodules/drawing');
+
+mongoose.connect(process.env.MONGOURL);
 
 app.use(cors({credentials:true,origin:process.env.ORIGIN}));
 app.use(express.json());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
-mongoose.connect(process.env.MONGOURL);
+app.post('/drawing', uploadMiddleware.single('file'), async (req,res) => {
+    const {originalname,path} = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    const newPath = path+"."+ext;
+    fs.renameSync(path, newPath);
+
+    const {title,artist} = req.body;
+    const postDoc = await Drawing.create({
+        title,
+        artist,
+        art:newPath,
+    })
+
+    res.json(postDoc);
+});
+
+app.get('/drawings', async(req,res) => {
+    res.json(await Drawing.find());
+});
 
