39f982a4f5f715cc09afd13e43a6dfdfac1d84a0
V3nusAsABoy <andrewalloniealt@gmail.com>
2026-01-09 21:00:23 -0500

    added id and div to drawingpage

--- react/src/DrawingPage.tsx
+++ react/src/DrawingPage.tsx
@@ -1,6 +1,9 @@
+import { useParams } from "react-router-dom"
+
 export default function DrawingPage(){
+    const {id} = useParams();
     return(
-        <>
-        </>
+        <div className="body">
+        </div>
     )
 }
